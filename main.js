4468723fb83a2055c974ddab75db868273d3ae2d
Dan Parkes <daniel.parkes901@gmail.com>
2017-04-26 20:29:15 +0100

    changed the nav bar, added a page, removed bootstrap

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
-$(document).ready(function(){
-    $('ul').trigger()
-})
+$(function() {
+    $('rotating').on("click", function() {
+        $("img").attr("src", "http://cuteimages.net/data/2015/11/not-your-everyday-superhero--photos-cuteimages.net.jpg");
+    });
+    $("superhero").on("click", function() {
+        $("img").attr("src", "http://cl.jroo.me/z3/v/N/0/d/a.baa-cat-batman.jpg");
+    });
+    $("superhero").on("click", function() {
+        $("img").attr("src", "http://cdn.hiconsumption.com/wp-content/uploads/2013/05/Cats-as-Superheroes-by-Jenny-Parks-5.jpg");
+    });
+});
